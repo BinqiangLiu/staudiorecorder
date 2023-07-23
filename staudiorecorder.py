2b3c59e6687f6f9a24bc9f0d2732081cb205d249
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-24 03:46:25 +0800

    Update staudiorecorder.py

--- staudiorecorder.py
+++ staudiorecorder.py
@@ -52,8 +52,11 @@ if len(audio) > 0:
 
    # Transcribe the audio using OpenAI API将录音文件转文本
 
-    stt_audio_file = open("audiorecorded.mp3", "rb")
-    transcript = openai.Audio.transcribe("whisper-1", stt_audio_file)
+#    stt_audio_file = open("audiorecorded.mp3", "rb")
+    model = whisper.load_model("base")
+    transcript = model.transcribe("audiorecorded.mp3")
+    
+#    transcript = openai.Audio.transcribe("whisper-1", stt_audio_file)
 #    text = transcript["text"]
 # Remove the temporary audio file
     os.remove("audiorecorded.mp3")    
