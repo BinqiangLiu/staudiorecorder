c250396f6c1d0542041863c268b657309e80d90d
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-23 23:46:50 +0800

    Update staudiorecorder.py

--- staudiorecorder.py
+++ staudiorecorder.py
@@ -24,39 +24,26 @@ conversation = [{"role": "system", "content": "You are an intelligent professor.
 
 st.title("Audio to Chat App")
 
-    # Audio input section
+# Audio input section语音输入部分
 st.header("Step 1: Speak to the AI")
 st.write("Click the Record Button below and speak to the AI.")
 
-audio_bytes = audio_recorder()
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
+audio = audiorecorder("点击开始录音", "点击停止录音")
 
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.tobytes())
+    
+    # To save audio to a file:
+    audio_file = open("audio.mp3", "wb")
+    audio_file.write(audio.tobytes())
 
-# Save the audio data to a WAV file
-#****************更换下面的语音转文字代码，主要是转化录音的格式
-    # Convert the audio data to a numpy array with one channel (mono)
-
-# Assume data is a tuple containing the audio data for a single-channel audio
-audio_file = "justnameit.wav"
-if len(audio_bytes) > 0:
-    audio_data = audio_bytes[0]  # Access the first element for a single-channel audio
-    sf.write(audio_file, audio_data, 44100, format="wav")
-else:
-    print("Error: The audio data （audio_bytes） is empty.")
-
-#audio_data = np.frombuffer(audio_bytes, dtype=np.int16)
-#audio_file = "justnameit.wav"
-#sf.write(audio_file, audio_bytes, 44100, format="wav")
-
-    # Transcribe the audio using OpenAI API
+# Transcribe the audio using OpenAI API将录音文件转文本
 with open(audio_file, "rb") as file:
     transcript = openai.Audio.transcribe("whisper-1", file)
-#    return transcript["text"]    
     text = transcript["text"]    
-            # Remove the temporary audio file
+# Remove the temporary audio file
 #    os.remove(audio_file)    
-#****************
 
 # Function to perform chat with OpenAI GPT-3
 def chat_with_openai(input_text):
@@ -78,20 +65,14 @@ def text_to_speech(text):
     os.remove("response.mp3")  # Remove the temporary audio file
     return response_audio
 
-    if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-
-        # Transcribe audio and perform chat
-        #with st.spinner("Processing..."):
-        #    text = transcribe_audio(audio_bytes)
 response = chat_with_openai(text)
 
-        # Display the chat history and play response audio
+# Display the chat history and play response audio
 st.header("Chat History")
 st.write("You: " + text)
 st.write("AI: " + response)
 
-        # Audio output section
+# Audio output section
 st.header("Step 2: Listen to the AI Response")
 st.audio(text_to_speech(response), format="audio/mp3", start_time=0)
 
