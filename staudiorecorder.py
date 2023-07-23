33c037bf8c6ec4e2908c1e156cb0d0617f65a36a
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-24 02:11:41 +0800

    Update staudiorecorder.py

--- staudiorecorder.py
+++ staudiorecorder.py
@@ -5,18 +5,19 @@
 #使用这个录音模块：https://github.com/theevann/streamlit-audiorecorder
 import streamlit as st
 from audiorecorder import audiorecorder
-#import subprocess
+import subprocess
 import openai
 import pyttsx3
 #import whisper 
 #import sounddevice as sd
-#import soundfile as sf
-#import numpy as np
+import soundfile as sf
+import numpy as np
 #from audio_recorder_streamlit import audio_recorder
 #运行的时候有报错sh:1: ffmpeg not found
-#import ffmpeg
-#import av
-#from pydub import AudioSegment
+import ffmpeg
+import av
+from pydub import AudioSegment
+import Pyaudio
 
 # Load environment variables
 from dotenv import load_dotenv
@@ -80,22 +81,17 @@ if len(audio) > 0:
     st.write("AI: " + system_message)
 
 # Function to convert text to speech using pyttsx3
-    def text_to_speech(text):
-        engine = pyttsx3.init()
-        engine.setProperty("rate", 150)
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 150)
 #    engine.setProperty("voice", "english-us")
-        engine.save_to_file(text, "response.mp3")
-        engine.runAndWait()
-        with open("response.mp3", "rb") as file:
-            response_audio = file.read()
-        os.remove("response.mp3")  # Remove the temporary audio file
-        return response_audio
-
-#response = chat_with_openai(transcript["text"])
-
+    engine.save_to_file(text, "response.mp3")
+    engine.runAndWait()
 # response audio output section
     st.header("Step 2: Listen to the AI Response")
-    st.audio(text_to_speech(response), format="audio/mp3", start_time=0)
+    st.audio("response.mp3", format="audio/mp3", start_time=0)
+#    os.remove("response.mp3")  # Remove the temporary audio file
+
+#response = chat_with_openai(transcript["text"])
 
 #if __name__ == "__main__":
 #    main()
