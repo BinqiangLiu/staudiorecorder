6c9b1367e70c6868224bbb6ab912564876d76ce0
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-31 10:17:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def text_to_speech(text):
     except Exception as e:
         # Handle the error, e.g., print an error message or return a default text
         print(f"Translation error: {e}")
-        return "提示：要翻译内容为空"
+        return "TTS RESULT ERROR"
         st.stop()
 
 if system_message is None:
@@ -126,6 +126,7 @@ else:
         st.write(transcript)
         st.write("AI Response")
         output_text = text_to_speech(system_message)
+        st.audio("translationresult.mp3")
         st.write(f" {output_text}")    
 #    if click_clear:
 #        text = placeholder.text_input(label="第三步：输入需要翻译的内容（请务必先输入要翻译的内容再查看翻译或播放语音）", value="", placeholder='在此输入Enter here', key=2)
@@ -137,6 +138,7 @@ else:
             audio_file = open("translationresult.mp3", "rb")
             audio_bytes = audio_file.read()
             st.audio("translationresult.mp3")
+            st.write(f" {output_text}")    
         except Exception as e:
             # Handle the error, e.g., print an error message or return a default text
             print(f"Translation error: {e}")            
